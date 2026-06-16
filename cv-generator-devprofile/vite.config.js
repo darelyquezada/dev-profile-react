7f29bb2a38fd37766c0ebb53760f873fc675f3df
darelyquezada <darelyqg@gmail.com>
2026-06-15 18:50:29 -0600

    Updating config files for deploy

--- cv-generator-devprofile/vite.config.js
+++ cv-generator-devprofile/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dev-profile-react/',
   plugins: [react()],
+  base: '/dev-profile-react/',
+  build: {
+    chunkSizeWarningLimit: 1200, 
+  },
 })
